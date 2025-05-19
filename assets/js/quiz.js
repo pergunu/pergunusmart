16eff6c44f5981d2a93bbe94efc78ac4118c61b3
cendhanu <pergunusitubondo@gmail.com>
2025-05-19 13:40:29 +0700

    Create quiz.js

---
+++ assets/js/quiz.js
@@ -0,0 +1,7 @@
+// Quiz-specific functions
+function initializeQuiz() {
+    // This function would be called from main.js to initialize quiz-specific functionality
+    console.log("Quiz initialized");
+}
+
+// Additional quiz-related functions can be added here
