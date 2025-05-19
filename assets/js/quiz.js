d84d9c581e34fa21e707f317763c631d6b51e981
cendhanu <pergunusitubondo@gmail.com>
2025-05-19 14:00:01 +0700

    Delete assets/js directory

--- assets/js/quiz.js
+++
@@ -1,7 +0,0 @@
-// Quiz-specific functions
-function initializeQuiz() {
-    // This function would be called from main.js to initialize quiz-specific functionality
-    console.log("Quiz initialized");
-}
-
-// Additional quiz-related functions can be added here
