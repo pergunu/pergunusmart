5deaa5817b51584d56eac30a0441d8644c6bb1c7
cendhanu <pergunusitubondo@gmail.com>
2025-05-21 12:43:31 +0700

    Delete assets directory

--- assets/js/particles.js
+++
@@ -1,3 +0,0 @@
-// This would be the particles.js library and configuration
-// You can download the library from https://github.com/VincentGarreau/particles.js/
-// Or use CDN: https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js
